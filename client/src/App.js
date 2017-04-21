ba955e1ca45cc1832a2ec5399e9ccc1c67e0a7c6
Mike McDonald <mikeraymcdonald@gmail.com>
2017-04-21 15:45:13 -0500

    Add app, home, react-router and customer list

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,23 @@
 import React, { Component } from 'react';
-import SelectedFoods from './SelectedFoods';
-import FoodSearch from './FoodSearch';
+import Header from './Header';
+import Footer from './Footer';
+
 
 class App extends Component {
-  state = {
-    selectedFoods: [],
-  }
 
-  removeFoodItem = (itemIndex) => {
-    const filteredFoods = this.state.selectedFoods.filter(
-      (item, idx) => itemIndex !== idx,
-    );
-    this.setState({ selectedFoods: filteredFoods });
-  }
-
-  addFood = (food) => {
-    const newFoods = this.state.selectedFoods.concat(food);
-    this.setState({ selectedFoods: newFoods });
-  }
+  state = {}
 
   render() {
-    const { selectedFoods } = this.state;
 
     return (
-      <div className='App'>
-        <div className='ui text container'>
-          <SelectedFoods
-            foods={selectedFoods}
-            onFoodClick={this.removeFoodItem}
-          />
-          <FoodSearch
-            onFoodClick={this.addFood}
-          />
+      <div>
+        <Header
+        />
+        <div className='App'>
+          {this.props.children}
         </div>
+        <Footer
+        />
       </div>
     );
   }
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import App from './App';
-import './index.css';
+import Home from './Home';
 import '../semantic/dist/semantic.min.css';
+import './index.css';
 
 ReactDOM.render(
-  <App />,
+   <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Home} />
+    </Route>
+  </Router>,
   document.getElementById('root'), // eslint-disable-line no-undef
 );
